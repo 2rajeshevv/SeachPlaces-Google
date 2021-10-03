84a1f81f974ee361185e389d8de5a1e10c7ef57c
2rajeshevv <2rajeshevv@gmail.com>
2021-10-03 07:05:55 +0530

    committing again for scheme updates

---
+++ Pods/Target Support Files/Pods-SeachPlaces-Google/Pods-SeachPlaces-Google-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeachPlaces_Google : NSObject
+@end
+@implementation PodsDummy_Pods_SeachPlaces_Google
+@end
---
+++ SeachPlaces-Google/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SeachPlaces-Google
+//
+//  Created by Evv Rajesh on 03/10/21.
+//
+
+import UIKit
+import GooglePlaces
+
+class ViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
